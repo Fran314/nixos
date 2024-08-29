d245bf43973bbf59807228a97d220fa58c36ec8d
Fran314 <francesco.ghog@gmail.com>
2024-08-29 12:14:24 +0200

    edited xdg

--- user/modules/xdg/default.nix
+++ user/modules/xdg/default.nix
@@ -1,15 +1,19 @@
 { config, pkgs, ...}:
 
 {
-  xdg.userDirs = {
-      enable = true;
-      desktop = "${config.home.homeDirectory}/desktop";
-      music = "${config.home.homeDirectory}/music";
-      videos = "${config.home.homeDirectory}/videos";
-      pictures = "${config.home.homeDirectory}/pictures";
-      download = "${config.home.homeDirectory}/download";
-      documents = "${config.home.homeDirectory}/documents";
-      publicShare = "${config.home.homeDirectory}/public";
-      templates = "${config.home.homeDirectory}/templates";
-  };
+    home.packages = with pkgs; [
+        xdg-user-dirs
+    ];
+
+    xdg.userDirs = {
+        enable = true;
+        desktop = "${config.home.homeDirectory}/desktop";
+        music = "${config.home.homeDirectory}/music";
+        videos = "${config.home.homeDirectory}/videos";
+        pictures = "${config.home.homeDirectory}/pictures";
+        download = "${config.home.homeDirectory}/download";
+        documents = "${config.home.homeDirectory}/documents";
+        publicShare = "${config.home.homeDirectory}/public";
+        templates = "${config.home.homeDirectory}/templates";
+    };
 }
