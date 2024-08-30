767f1ed66edfc7ede6aa104026c45a7249d6b1b8
Fran314 <francesco.ghog@gmail.com>
2024-08-30 12:21:18 +0200

    fixed 3d modeling / octave mixup

--- user/modules/productivity/3d-modeling/default.nix
+++ user/modules/productivity/3d-modeling/default.nix
@@ -2,10 +2,14 @@
 
 {
     home.packages = with pkgs; [
-        (octaveFull.withPackages (ps: with ps; [
-            # Search for `octavePackages` in NixOS packages to see all the possible options
-            # Once in octave, load the package with `pkg load <name-of-the-package>`
-            nurbs
-        ]))
+        prusa-slicer
+
+        # I would very much like to add freecad here, but at the time of
+        # writing the only available version in nix packages (both stable and
+        # unstable) is 0.21.*, while weekly builds for 0.22.0dev exist. For now
+        # it's best to download the weekly dev build as AppImage and run it.
+        # When the 1.0 will come (which should be the version that 0.22 will
+        # become) and when it will be added to nix packages, I'll happily add
+        # it here
     ];
 }
