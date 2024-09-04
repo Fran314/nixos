441241b763685a76c5073ae48911597d83cd2843
Fran314 <francesco.ghog@gmail.com>
2024-09-04 17:10:56 +0200

    removed hardware configuration from tracking because it makes no sense to track it

--- profiles/vm/configuration.nix
+++ profiles/vm/configuration.nix
@@ -7,9 +7,11 @@
     ];
 
     # Bootloader.
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sda";
-    boot.loader.grub.useOSProber = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    # boot.loader.grub.enable = true;
+    # boot.loader.grub.device = "/dev/sda";
+    # boot.loader.grub.useOSProber = true;
 
     networking.hostName = "vm";
 
