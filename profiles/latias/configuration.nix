13011b02440c19148420b2ca644fdc69c7f89903
Fran314 <francesco.ghog@gmail.com>
2024-12-27 21:57:28 +0100

    failed attempt at making steam not run in background when closed

--- profiles/latias/configuration.nix
+++ profiles/latias/configuration.nix
@@ -58,6 +58,12 @@
     # Steam stuff
     programs.steam = {
         enable = true;
+        # # Attempt at making steam not running in background when closed. Sadly it seems that this flag has been removed
+        # package = pkgs.steam.override {
+        #     extraEnv = {
+        #         STEAM_FRAME_FORCE_CLOSE=true;
+        #     };
+        # };
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
         localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
