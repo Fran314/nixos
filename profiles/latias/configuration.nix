d6296c2e5eb8cbba83670e85c304285941c64dcc
Fran314 <francesco.ghog@gmail.com>
2025-01-09 00:39:40 +0100

    added precisation to docker

--- profiles/latias/configuration.nix
+++ profiles/latias/configuration.nix
@@ -63,6 +63,8 @@
     services.libinput.touchpad.tappingDragLock = false;
 
     virtualisation.docker.enable = true;
+    # # Needed if you're not using docker in rootless mode
+    # # but not necessary since we're enabling rootless mode
     # users.users.baldo.extraGroups = [ "docker" ];
     virtualisation.docker.rootless = {
         enable = true;
