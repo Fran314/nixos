6a7970024b3715f3c16f4297c12e56cffb077224
Fran314 <francesco.ghog@gmail.com>
2025-01-18 19:27:36 +0100

    updated scripts: added python to new-project

--- modules/nvim/nvim/options.lua
+++ modules/nvim/nvim/options.lua
@@ -8,7 +8,8 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
+vim.opt.autoindent = true
 
 vim.opt.smartindent = true
 
@@ -28,4 +29,3 @@ vim.opt.isfname:append("@-@")
 vim.o.fillchars = "eob: "
 ------------------------------
 --
-
