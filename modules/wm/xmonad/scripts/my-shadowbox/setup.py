0755327e502931b688912c94f8586cbbc0bd2646
Fran314 <francesco.ghog@gmail.com>
2025-01-21 15:37:30 +0100

    fixed python scripts author whoopsies

--- modules/wm/xmonad/scripts/my-shadowbox/setup.py
+++ modules/wm/xmonad/scripts/my-shadowbox/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='@pname@',
     version='@version@',
-    author='deter0',
+    author='Fran314',
     description='@desc@',
     install_requires=['pycairo', 'requests', 'PyGObject'],
     scripts=[
