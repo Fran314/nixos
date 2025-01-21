938a4f85a43398b6eb4fda0acc3f6b4ab331bed6
Fran314 <francesco.ghog@gmail.com>
2025-01-21 15:40:10 +0100

    nvim: removed autowrap on markdown

--- modules/nvim/nvim/autowrap.lua
+++ modules/nvim/nvim/autowrap.lua
@@ -1,11 +1,11 @@
 ---------- AUTOWRAP ----------
-local autowrap_group = vim.api.nvim_create_augroup("Markdown Wrap Settings", { clear = true })
+local autowrap_group = vim.api.nvim_create_augroup("Auto Wrap Settings", { clear = true })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*.md",
-	group = autowrap_group,
-	command = "setlocal wrap",
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+-- 	pattern = "*.md",
+-- 	group = autowrap_group,
+-- 	command = "setlocal wrap",
+-- })
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = "*.tex",
 	group = autowrap_group,
@@ -13,4 +13,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 ------------------------------
 --
-
