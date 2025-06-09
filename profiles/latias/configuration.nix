d32bdea09274eeda132c2e79a9f9aed413760c2c
Fran314 <francesco.ghog@gmail.com>
2025-06-10 01:02:41 +0200

    added symbolic package to octave

--- profiles/latias/configuration.nix
+++ profiles/latias/configuration.nix
@@ -152,6 +152,7 @@
         # Search for `octavePackages` in NixOS packages to see all the possible options
         # Once in octave, load the package with `pkg load <name-of-the-package>`
         nurbs
+        symbolic
       ]
     ))
   ];
