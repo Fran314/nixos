51a07ae903d613cf9ea392f9970ea027073b5667
Fran314 <francesco.ghog@gmail.com>
2025-07-16 11:26:14 +0200

    created secrets for home wireguard

--- modules/secrets-manager/default.nix
+++ modules/secrets-manager/default.nix
@@ -6,7 +6,7 @@
 let
   secrets-manager = pkgs.callPackage (builtins.fetchGit {
     url = "https://github.com/Fran314/secrets-manager-rs";
-    rev = "4089f4c6b4dd6d69b376e7842a476fa6fae441fd";
+    rev = "521423fa544a68d0341bd3479c3eeb4002a92193";
   }) { };
 in
 {
