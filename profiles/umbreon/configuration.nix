2141fb0569c2713cb32aa456ed3430d6503baed5
Fran314 <francesco.ghog@gmail.com>
2026-01-07 02:08:10 +0100

    added autoscrub to umbreon

--- profiles/umbreon/configuration.nix
+++ profiles/umbreon/configuration.nix
@@ -100,6 +100,7 @@
   boot.zfs.devNodes = "/dev/disk/by-path";
   boot.zfs.extraPools = [ "hm01" ];
   networking.hostId = "6adbf918"; # The primary use case is to ensure when using ZFS that a pool isn’t imported accidentally on a wrong machine
+  services.zfs.autoScrub.enable = true;
 
   home-manager.users.baldo =
     { config, pkgs, ... }:
